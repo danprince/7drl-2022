85d42b31ed1dff6f544bf9b7950e90a40c5908d4
Dan Prince <danprince@hey.com>
2022-03-09 12:05:02 +0100

    tweaks

--- src/levels.ts
+++ src/levels.ts
@@ -1,8 +1,8 @@
-import { RNG, Direction } from "silmarils";
 import { LevelType } from "./game";
-import { LevelBuilder, Symmetry, TileMarker } from "./builders";
+import { TileMarker } from "./builders";
 import * as Tiles from "./tiles";
 import * as Entities from "./entities";
+import { RNG } from "silmarils";
 
 export let PrimordialCaverns = new LevelType({
   name: "Primordial Caverns",
@@ -15,19 +15,21 @@ export let PrimordialCaverns = new LevelType({
   rareEntities: [],
   build(builder) {
     return builder
-      .fill(TileMarker.Wall)
-      .noise(0.4)
+      //.fill(TileMarker.Floor)
+      //.addPerimeterWall()
+      .noise(RNG.float(0.3, 0.5))
       .cellularAutomata({
         iterations: 20,
-        rules: [[6, 7, 8], [3, 4, 5, 6, 7, 8]],
+        rules: [[5, 6, 7, 8], [3, 4, 5, 6, 7, 8]],
         outOfBoundsMarker: TileMarker.Wall,
       })
+      .createEntrance()
+      .createExit()
+      // TODO: Is mapper needed here if we have the floorTiles/wallTiles above?
       .build(marker => {
         switch (marker) {
           case TileMarker.Wall:
             return Tiles.Wall;
-          case TileMarker.Exit:
-            return Tiles.Doorway;
           case TileMarker.Floor:
             return Tiles.Floor;
         }
